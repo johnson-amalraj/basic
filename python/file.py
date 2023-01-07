9f38597226689446d0bfb356f307d6c14f6da3b9
johnson-amalraj <johnsonamalraj@outlook.com>
2023-01-07 12:38:09 +0530

    some minor changes

---
+++ python/file.py
@@ -0,0 +1,1 @@
+# Do some File access and with that file do some operations
--- python/magic.py
+++ python/magic.py
@@ -1,2 +1,23 @@
  # Need to implement some magic operation
  # Like some dynamic printing
+
+# ---------------------------------
+# Import required packages 
+# ---------------------------------
+import os
+
+# ---------------------------------
+# Variable Declaration
+# ---------------------------------
+user_name = os.getlogin(); 
+# str.upper(user_name)
+
+# ---------------------------------
+# Main Program
+# ---------------------------------
+print("Hello,", user_name + "! How are you?")
+password = input("Enter Password:")
+
+
+# TODO check the password and compare then give access
+# and then get the string and print some dynamic text
